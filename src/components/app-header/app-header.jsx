18066f371e5a6dd438a24ba57619d6e3e41b935d
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-06-22 14:05:59 +0400

    feature: header styles is more BEM

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -12,23 +12,23 @@ class AppHeader extends React.Component {
   render() {
     return (
       <header className={cn(styles.header, 'p-4')}>
-        <div className={styles.headerContainer}>
-          <nav className={cn(styles.headerBox)}>
+        <div className={styles.header__container}>
+          <nav className={cn(styles.header__box)}>
             <ul className={styles.list}>
-              <li className={styles.listItem}>
+              <li className={styles.list__item}>
                 <a href='/constructor' className={cn(styles.link, 'mt-4 mr-5 mb-4 ml-5')}>
                   <BurgerIcon type={'primary'} />
-                  <label className={cn(styles.linkLabel, 'text text_type_main-default', 'ml-2')}>
+                  <label className={cn(styles.link__label, 'text text_type_main-default', 'ml-2')}>
                     Конструктор
                   </label>
                 </a>
               </li>
-              <li className={cn(styles.listItem, 'ml-2')}>
+              <li className={cn(styles.list__item, 'ml-2')}>
                 <a href='/orders' className={cn(styles.link, 'mt-4 mr-5 mb-4 ml-5')}>
                   <ListIcon type='secondary' />
                   <label
                     className={cn(
-                      styles.linkLabel,
+                      styles.link__label,
                       'text text_type_main-default',
                       'text_color_inactive',
                       'ml-2'
@@ -39,16 +39,16 @@ class AppHeader extends React.Component {
               </li>
             </ul>
           </nav>
-          <div className={cn(styles.headerBox)}>
+          <div className={cn(styles.header__box)}>
             <Logo />
           </div>
-          <div className={cn(styles.headerBox)}>
-            <div className={styles.linkWrap}>
+          <div className={cn(styles.header__box)}>
+            <div className={styles.link__wrap}>
               <a href='/profile' className={cn(styles.link, 'mt-4 mr-5 mb-4 ml-5')}>
                 <ProfileIcon type='secondary' />
                 <label
                   className={cn(
-                    styles.linkLabel,
+                    styles.link__label,
                     'text text_type_main-default',
                     'text_color_inactive',
                     'ml-2'
