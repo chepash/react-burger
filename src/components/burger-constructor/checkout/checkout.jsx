3712cd476a281a26412349e3198d3d4bb4bb6b3d
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-06-23 14:23:56 +0400

    checkout component now func component

--- src/components/burger-constructor/checkout/checkout.jsx
+++ src/components/burger-constructor/checkout/checkout.jsx
@@ -3,20 +3,18 @@ import cn from 'classnames';
 import styles from './checkout.module.scss';
 import { Button, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
-class Checkout extends React.Component {
-  render() {
-    return (
-      <div className={cn(styles.checkout, 'mt-10', 'pr-4')}>
-        <p className={cn(styles.checkout__price, 'text text_type_digits-medium', 'mr-10')}>
-          610
-          <CurrencyIcon type='primary' />
-        </p>
-        <Button htmlType='button' type='primary' size='large'>
-          Оформить заказ
-        </Button>
-      </div>
-    );
-  }
-}
+const Checkout = () => {
+  return (
+    <div className={cn(styles.checkout, 'mt-10', 'pr-4')}>
+      <p className={cn(styles.checkout__price, 'text text_type_digits-medium', 'mr-10')}>
+        610
+        <CurrencyIcon type='primary' />
+      </p>
+      <Button htmlType='button' type='primary' size='large'>
+        Оформить заказ
+      </Button>
+    </div>
+  );
+};
 
 export default Checkout;
