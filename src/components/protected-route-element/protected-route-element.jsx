ce4640f1b690ec45d66046bafc71023131e78a25
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-07-24 22:23:15 +0400

    refactor ProtectedRouteElement

--- src/components/protected-route-element/protected-route-element.jsx
+++ src/components/protected-route-element/protected-route-element.jsx
@@ -1,32 +1,17 @@
-import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Navigate, useLocation } from 'react-router-dom'
 import { SET_REDIRECT_PATH } from '../../services/actions/login-actions'
-import { getUser } from '../../services/actions/user-actions'
 
 export function ProtectedRouteElement({ element }) {
   const dispatch = useDispatch()
-  const [isUserLoaded, setUserLoaded] = useState(false)
   const location = useLocation()
-
-  const init = async () => {
-    await dispatch(getUser()).then(() => setUserLoaded(true))
-  }
 
   const isLoggedIn = useSelector((store) => store.userState.isLoggedIn)
 
-  useEffect(() => {
-    init()
-  }, [])
-
-  if (!isUserLoaded) {
-    return null
-  }
-
-  if (!isLoggedIn) {
+  if (isLoggedIn) {
+    return element
+  } else {
     dispatch({ type: SET_REDIRECT_PATH, payload: location.pathname })
     return <Navigate to="/login" />
   }
-
-  return isLoggedIn ? element : <Navigate to="/login" />
 }
