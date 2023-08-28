f16414d8fbe991a4af7033173b0d0f98106b89e9
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-08-28 14:11:18 +0400

    feet: add component profile-orders layout

--- src/components/profile/profile-orders/profile-orders.tsx
+++ src/components/profile/profile-orders/profile-orders.tsx
@@ -1,13 +1,34 @@
 import cn from 'classnames'
 import styles from './profile-orders.module.scss'
 import { FC } from 'react'
+import OrderCard from '../../order-card/order-card'
 
 const ProfileOrders: FC = () => {
   return (
-    <section
-      aria-label="История заказов"
-      className={cn(styles.section)}
-    ></section>
+    <section aria-label="История заказов" className={cn(styles.section)}>
+      <div className={cn(styles.section_scrollable)}>
+        <ul className={cn(styles.list)}>
+          <li>
+            <OrderCard />
+          </li>
+          <li>
+            <OrderCard />
+          </li>
+          <li>
+            <OrderCard />
+          </li>
+          <li>
+            <OrderCard />
+          </li>
+          <li>
+            <OrderCard />
+          </li>
+          <li>
+            <OrderCard />
+          </li>
+        </ul>
+      </div>
+    </section>
   )
 }
 
