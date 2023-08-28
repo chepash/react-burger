4354546ccb5f5906fd98de7bc291ff509de61565
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-08-28 22:28:54 +0400

    feat: add profile orders layout with mock data

--- src/components/profile/profile-orders/profile-orders.tsx
+++ src/components/profile/profile-orders/profile-orders.tsx
@@ -1,31 +1,19 @@
 import cn from 'classnames'
-import styles from './profile-orders.module.scss'
 import { FC } from 'react'
+import { useSelector } from '../../../services/types/store'
 import OrderCard from '../../order-card/order-card'
+import styles from './profile-orders.module.scss'
 
 const ProfileOrders: FC = () => {
+  const orderHistory = useSelector((store) => store.feedState.response.orders)
+
   return (
     <section aria-label="История заказов" className={cn(styles.section)}>
       <div className={cn(styles.section_scrollable)}>
         <ul className={cn(styles.list)}>
-          <li>
-            <OrderCard />
-          </li>
-          <li>
-            <OrderCard />
-          </li>
-          <li>
-            <OrderCard />
-          </li>
-          <li>
-            <OrderCard />
-          </li>
-          <li>
-            <OrderCard />
-          </li>
-          <li>
-            <OrderCard />
-          </li>
+          {orderHistory.map((order) => (
+            <OrderCard key={order._id} order={order} />
+          ))}
         </ul>
       </div>
     </section>
