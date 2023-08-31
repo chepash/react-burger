fa10102c85f6b3f12ff8f436eb3fe2024bb7aac5
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-08-31 22:31:05 +0400

    fix: days endings in formatUpdatedAtTime

--- src/utils/utils-functions.ts
+++ src/utils/utils-functions.ts
@@ -20,7 +20,10 @@ export const formatUpdatedAtTime = (updatedAt: string): string => {
   const getDaysText = (count: number): string => {
     if (count === 1) {
       return 'день'
-    } else if (count >= 2 && count <= 4) {
+    } else if (
+      (count % 10 === 2 || count % 10 === 3 || count % 10 === 4) &&
+      (count < 10 || count > 20)
+    ) {
       return 'дня'
     } else {
       return 'дней'
